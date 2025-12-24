e4108ecad2af264a6603ed578c14ad843053249f
DarshilSingh16 <darsh35sin@gmail.com>
2025-12-24 11:59:20 +0530

    readme deletion

--- Assignment.py
+++ Assignment.py
@@ -6,8 +6,8 @@ print("Addition: ", a+b)
 print("Subtraction: ", a-b)
 print("Multiplication: ", a*b)
 print("Division: ", a/b)
-print('New Edit')
 
 x = input("Enter your first name: ")
 y = input("Enter your second name: ")
 print("Hello,",x,y, "! Welcome to the python program. ")
+
